9abb75cdcffaf6e75ba601711d5bc8fe1f84182d
Aniruddha Pawar <aniruddha662003@gmail.com>
2023-10-01 18:07:13 +0530

    vercel.json and atlas connect

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://0.0.0.0:27017/iNotebook"
-
+// const mongoURI = "mongodb://0.0.0.0:27017/iNotebook"
+const mongoURI = "mongodb+srv://aniruddha:aniruddha@inotebook.hfyzhli.mongodb.net/?retryWrites=true&w=majority"
 mongoose.set('strictQuery', false);
 const connetToMongo =()=>{
     mongoose.connect(mongoURI,()=>{
